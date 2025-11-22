3e8be7fa6025109bc2d908c1f700f8d0f411b0f2
HIMANSHU KUMBHAJ <himanshukumbhaj0@gmail.com>
2025-11-22 22:50:58 +0530

    Delete Chapter 5 directory

--- Chapter 5/Do it Yourself/NoUsers.py
+++
@@ -1,7 +0,0 @@
-users = []
-
-for user in users:
-    print(f"Hello {user}, thankyou for logging in again!")
-else:
-    print("We need to find some users!")
-
--- Chapter 5/Topics/voting.py
+++
@@ -1,7 +0,0 @@
-age = 17
-if age >= 18:
-    print("You are old enough to vote.")
-    print("Have you registered to vote yet!")
-else:
-    print("Sorry you are to young to vote!")
-    print("Go play with barbie")
