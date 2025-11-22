64d5f78e6e341aa8621886d83e5db8754d3a8846
HIMANSHU KUMBHAJ <himanshukumbhaj0@gmail.com>
2025-11-22 22:51:13 +0530

    Delete Chapter 7 directory

--- Chapter 7/Do it Yourself/MultiplesOfTen.py
+++
@@ -1,7 +0,0 @@
-num = input("Enter a number i'll tell you if its multiple of 10 or not.\n-> ")
-num = int(num)
-
-if num % 10 == 0:
-    print(f"{num} is multiple of 10")
-else:
-    print(f"{num} is not a multiple of 10")
--- Chapter 7/Do it Yourself/RestaurantSearing.py
+++
@@ -1,7 +0,0 @@
-table = input("How many people are thier for dinner group.\n-> ")
-table = int(table)
-
-if table >= 8:
-    print("Appologies, you have to wait for a table")
-else:
-    print("Table is ready.")
